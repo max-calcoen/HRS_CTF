2cccaf2a870760b68611b2b32081188ebe5a55fb
Max Calcoen <maxcalcoen@gmail.com>
2023-11-29 10:19:34 -0800

    downloads done, next step integrate db

--- app.py
+++ app.py
@@ -1,22 +1,28 @@
-from flask import Flask  # type: ignore
+from fileinput import filename
+import os
+from flask import Flask, current_app, send_from_directory  # type: ignore
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="")
 
 
 @app.route("/")
 def home():
+    return send_from_directory("static", "index.html")
+
+
+@app.route("/login", methods=["POST"])
+def login():
     pass
 
 
-@app.route("/leaderboard")
-def leaderboard():
-    pass
-
-
-@app.route("/profile")
-def profile():
-    pass
+@app.route("/file_request/<path:filename>", methods=["POST"])
+def file_request(filename):
+    uploads = os.path.join(current_app.root_path, "server_resources")
+    file_path = os.path.join(uploads, filename)
+    if not os.path.exists(file_path):
+        return "File not found", 404
+    return send_from_directory(uploads, filename, as_attachment=True)
 
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    app.run(port=8080, debug=True)

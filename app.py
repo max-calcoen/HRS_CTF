30cad81e664ca3e11e4725df71802a4427cbcb6b
Max Calcoen <maxcalcoen@gmail.com>
2023-12-01 09:55:50 -0800

    redis shenanigans

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 from fileinput import filename
 import os
-from flask import Flask, current_app, send_from_directory, session, request, jsonify  # type: ignore
+from flask import Flask, current_app, send_from_directory, session, request, jsonify, redirect  # type: ignore
 import secrets
 import sqlite3
 import bcrypt
+import redis
 
 app = Flask(__name__, static_url_path="")
 app.secret_key = secrets.token_hex()
 
+# connect to redis
+redis_client = redis.StrictRedis(
+    host="localhost", port=6379, db=0, decode_responses=True
+)
 
+
+# index.html override
 @app.route("/")
 def home():
     return send_from_directory("static", "index.html")
@@ -16,6 +23,7 @@ def home():
 
 @app.route("/signup", methods=["POST"])
 def signup():
+    global redis_client
     # check if user is already signed in
     if "username" in session:
         return jsonify({"error": "User is already signed in"}), 400
@@ -30,6 +38,7 @@ def signup():
     )
     user = cursor.fetchone()
     if user is not None:
+        connection.close()
         return jsonify({"error": "User already exists"}), 400
 
     hashed_password = bcrypt.hashpw(
@@ -40,8 +49,13 @@ def signup():
         (request.form["username"], hashed_password),
     )
     connection.commit()
+    connection.close()
     # TODO: sign in with their info and add to session
-    return jsonify({"message": "User created successfully"}), 201
+    session_token = os.urandom(24).hex()
+    redis_client.set(session_token, user[0])
+    session["token"] = session_token
+    # user successfully created account, display home page
+    return redirect("/")
 
 
 @app.route("/signin", methods=["POST"])
@@ -84,6 +98,13 @@ def file_request(filename):
 def request_server():
     # TODO: start server with port not taken, authenticate the request with session info, set timer to close
     pass
+
+
+def is_logged_in():
+    global redis_client
+    session_token = session.get("token")
+    # check if token exists and matches session cache
+    return session_token is not None and redis_client.get(session_token) is not None
 
 
 if __name__ == "__main__":
